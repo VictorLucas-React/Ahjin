79d7559208fb93eb19e916fd06d6792032f88f05
VictorLucas-React <victorlucas.vl51.vl@gmail.com>
2023-02-15 20:18:04 -0300

    finalizando formulario

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,10 +1,24 @@
 import Navbar from "../../components/Navbar";
+import {useState, UseState} from 'react'
 
 const Contact = () =>{
+    const [data, setdata] = useState({})
+    const submitFormData = (e) => {
+        e.preventDefault()
+        console.log (data)
+    }
+
     return(
         <div>
             <Navbar />
             <h1>Contato</h1>
+            <form onSubmit={submitFormData}>
+                <input type='text' placeholder="Nome" onChange={e=> setdata({...data, nome: e.target.value})}/>
+                <input type='text' placeholder="Email" onChange={e=> setdata({...data, email: e.target.value})}/>
+                <input type='text' placeholder="Telefone" onChange={e=> setdata({...data, telefone: e.target.value})}/>
+                <input type='submit' value="Cadastrar"/>
+            </form>
+
         </div>
         
 
