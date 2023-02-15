dd26abcfee9f44fb9c3b88da4c5907fd2458628f
VictorLucas-React <victorlucas.vl51.vl@gmail.com>
2023-02-15 19:58:50 -0300

    Implementando componentes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,23 @@
-import Navbar from '../../components/Navbar';
+import Navbar from '../../components/Navbar'
+import { useState } from 'react';
+import Loader from '../../components/Loader';
 
 const Home = () =>{
+    const[isLoard, setIsLoad] = useState(false);
+    const hadleStateComponet = () => {
+        setIsLoad (!isLoard)
+    }
+
     return(
        <div>
            <Navbar />
-        <h1>Home</h1>
+        <h1>Home</h1>        
+        <Loader load={isLoard} />
+        {isLoard ? (<p>Carregando...</p>): (<p></p>)}
+        <p>Componente rederiza conforme o estado</p>
+        <button onClick={hadleStateComponet}>Mudar estado</button>
        </div>
+       
 
     )
 }
